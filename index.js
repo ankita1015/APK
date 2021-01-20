99cef04f73fdc85dc2bb2de7168c050d0552352a
Priyasigh <72592223+Priyasigh@users.noreply.github.com>
2021-01-20 21:05:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+//public STATIC PATH
+app.use(express.static(path.join(__dirname, '/public/')))
+//set Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`server runnign at ${PORT}`))
