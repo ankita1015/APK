1ec0a24bc745c1d6bbe1a8bd8599d687f116f1db
kunjan1005 <barotkunjan1312@gmail.com>
2021-01-28 20:55:32 +0530

    ok

--- public/file.js
+++ public/file.js
@@ -1,14 +1,30 @@
-function readURL(input) {
-  if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-          $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-          $('#imagePreview').hide();
-          $('#imagePreview').fadeIn(650);
-      }
-      reader.readAsDataURL(input.files[0]);
+let Src;
+
+let divArray=[
+  document.getElementById('file-ip-1-preview'),
+  document.getElementById('file-ip-2-preview'),
+  document.getElementById('file-ip-3-preview'),
+  document.getElementById('file-ip-4-preview'),
+  document.getElementById('file-ip-5-preview')
+]
+
+let count=0;
+let imgArray=Array();
+function showpreview(event){
+  if(count<5){
+
+    Src=URL.createObjectURL(event.target.files[0]);
+  
+    divArray[count].src=Src;
+    divArray[count].style.display='block';
+    imgArray.push(Src);
+    
   }
+  console.log(imgArray);
+  count++;
+ 
+
+
 }
-$("#imageUpload").change(function() {
-  readURL(this);
-});
+
+
