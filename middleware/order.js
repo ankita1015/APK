bd73c5c4f8bfbc19d96957edea995eb15b355fe1
Priyasigh <pq9909312@gmail.com>
2021-03-05 08:45:58 +0530

    x

--- middleware/order.js
+++ middleware/order.js
@@ -1,25 +1,10 @@
 "use strict"
 const customerDocument=require('../modals/customermodal');
 
-
+// hii
 const order=async(req,res,next)=>{
     try{
         const user=req.user;
-<<<<<<< HEAD
-    
-        await customerDocument.find({userId:user._id}).exec((err,data)=>{
-
-        
-            
-        if(data.length!=0){
-     
-        req.customer=data;
-        next();
-        }else{
-            res.render('make-order');  
-        }
-    });
-=======
         
         const customer=await customerDocument.find({userId:user._id}).exec((err,data)=>{
           
@@ -33,7 +18,6 @@ const order=async(req,res,next)=>{
             }
         });
        
->>>>>>> d213adf29eb06abf33f7964183e7c838558bc8a6
       
     }catch(err){
         res.render('make-order');
