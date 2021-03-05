3201f2daed899218f4bd215c3bbd2617119b84c3
Priyasigh <pq9909312@gmail.com>
2021-03-05 21:59:36 +0530

    m

--- admin-controller/delete-product.js
+++ admin-controller/delete-product.js
@@ -1,9 +1,20 @@
 const productDocument=require('../modals/productsModal');
-
+const path=require('path');
+const fs=require('fs');
 module.exports=async(req,res)=>{
     try{
         let id=req.body.id;
-        const removed= await productDocument.deleteOne({_id:id});
+        let pt= path.join(__dirname,'./public');
+        const product=await productDocument.find({_id:id}).exec((err,data)=>{
+            // console.log(data);
+            data[0].images.forEach(element=>{
+                pt=pt.replace('\\','/');
+                console.log(pt);
+                fs.unlinkSync(`${element}`)
+
+            })
+        });
+        // const removed= await productDocument.deleteOne({_id:id});
        
         if(removed){
          res.send('1');  
