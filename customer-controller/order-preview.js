62e10bf02932457644549ddb9dded11c08dc99fe
kunjan1005 <barotkunjan1312@gmail.com>
2021-03-05 20:53:45 +0530

    ok

--- customer-controller/order-preview.js
+++ customer-controller/order-preview.js
@@ -5,11 +5,11 @@ module.exports=async(req,res)=>{
     try{
         let id=req.query.id;
          let uid=req.user;
-        //  console.log(uid)
+
         await productDocument.find({_id:id}).exec(async(err,product)=>{
                   await customerDocument.find({userId:uid._id}).exec((err,customerData)=>{
-console.log(product);
-                    res.render('order',{
+
+                    res.render('confirm-order',{
                         customerData,
                         product,
                     })
