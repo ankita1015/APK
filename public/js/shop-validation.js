4192ab1f5f2e73a8f3fe706f4cb27ab707592ec0
Priyasigh <pq9909312@gmail.com>
2021-03-21 19:27:57 +0530

    p

---
+++ public/js/shop-validation.js
@@ -0,0 +1,134 @@
+$(".save").click(function(){
+    var name=$('#name').val();  
+    var email=$('#email').val()
+    var password=$('#password').val()
+    var cpassword=$('#re_password').val()
+
+    var letters = /^[a-z][a-z\s]*$/;
+   var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      $(document).on("click",".error-icon",function(){
+      $('#e-error').css("display","none");
+      })
+       if(name=='' || name==undefined){
+          document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+          style='margin-right:45%;cursor:pointer'>X</span><span>Name can't be blank</span>`;
+          $('#e-error').css("display","block");  
+             return
+       }else if(!name.match(letters)){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>Name must be in Alphabetical only</span>`;
+         $('#e-error').css("display","block");  
+           return
+       }
+       if(email=='' || email==undefined){
+           document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+          style='margin-right:45%;cursor:pointer'>X</span><span>Email can't be blank</span>`;
+          $('#e-error').css("display","block");  
+             return
+       } else if(!filter.test(email)){
+           document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+          style='margin-right:45%;cursor:pointer'>X</span><span> Invalid email id</span>`;
+          $('#e-error').css("display","block");  
+             return
+       }
+       if(password=='' || password==undefined){
+           document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+          style='margin-right:45%;cursor:pointer'>X</span><span>Password can't be blank</span>`;
+          $('#e-error').css("display","block");  
+              return
+        }
+       if(cpassword=='' || cpassword==undefined){
+           document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+          style='margin-right:45%;cursor:pointer'>X</span><span>Confirm-password can't be blank</span>`;
+          $('#e-error').css("display","block");    
+              return
+        }
+
+         if(password!=cpassword){
+              document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+          style='margin-right:45%;cursor:pointer'>X</span><span>password & confirm-password must be same </span>`;
+          $('#e-error').css("display","block");  
+              return
+        }
+       let role=1
+       $.ajax({
+               url:'/',
+               type:"POST",
+               data:{name,email,password,cpassword,role},
+               success:function(data){
+      if(data==1){  
+      $('#name').val("")  
+      $('#email').val("")
+      $('#password').val("")
+      $('#re_password').val("")    
+          $('.sw-btn-next').show();
+      }
+
+
+               }
+           });
+      });
+      var shopname=$('#shopname').val()
+      var address1=$('#address1').val()
+      var address2=$('#address2').val()
+      var city=$('#city').val()
+      var state=$('#state').val()
+      var code=$('#code').val()
+      var mobileNo=$('#mobileNo').val()
+ 
+      var gst=$('#gst').val()
+
+      $('#shopname').keyup(function)
+      $
+    $('.create-account').click(function(e){
+      e.preventDefault()
+
+     
+    if(shopname=='' || shopname==undefined){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>Shop Name can't be blank</span>`;
+        $('#e-error').css("display","block");  
+        return
+    }
+    if(address1=='' || address1==undefined){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>Shop Address can't be blank</span>`;
+        $('#e-error').css("display","block");  
+        return
+    }
+    if(city =='' || city==undefined){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>city can't be blank</span>`;
+        $('#e-error').css("display","block");  
+        return
+    }
+    if(state =='' || state==undefined){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>State can't be blank</span>`;
+        $('#e-error').css("display","block");  
+        return
+    }
+    if(code=='' || code==undefined){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>Area code can't be blank</span>`;
+        $('#e-error').css("display","block");  
+        return
+    }
+    if(mobileNo =='' || mobileNo==undefined){
+        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:45%;cursor:pointer'>X</span><span>Mobile Number can't be blank</span>`;
+        $('#e-error').css("display","block");  
+        return
+    }
+
+       $.ajax({
+           url:'/shop',
+           type:'POST',
+           data:{shopname,address1,address2,city,state,code,gst,mobileNo},
+           success:function(data){
+         window.location.assign('/ourorders'); 
+           }
+       })
+  })
+
+
