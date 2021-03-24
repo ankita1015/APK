b68ac2112601d0c1a02e1c7cd2fbe878239a2da1
kunjan1005 <barotkunjan1312@gmail.com>
2021-03-24 16:46:38 +0530

    ok

--- public/js/shop-validation.js
+++ public/js/shop-validation.js
@@ -1,52 +1,60 @@
+
+$(document).on('click','.sw-btn-next',function(){
+
+    
+});
+
+
+
 $(".save").click(function(){
     var name=$('#name').val();  
     var email=$('#email').val()
     var password=$('#password').val()
     var cpassword=$('#re_password').val()
 
-    var letters = /^[a-z][a-z\s]*$/;
+    var letters = /^[A-Za-z\s]+$/;
    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
       $(document).on("click",".error-icon",function(){
       $('#e-error').css("display","none");
       })
        if(name=='' || name==undefined){
           document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-          style='margin-right:45%;cursor:pointer'>X</span><span>Name can't be blank</span>`;
+          style='margin-right:40%;cursor:pointer'>X</span><span>Name can't be blank</span>`;
           $('#e-error').css("display","block");  
              return
        }else if(!name.match(letters)){
         document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>Name must be in Alphabetical only</span>`;
+        style='margin-right:40%;cursor:pointer'>X</span><span>Name must be in Alphabetical only</span>`;
          $('#e-error').css("display","block");  
            return
        }
        if(email=='' || email==undefined){
            document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-          style='margin-right:45%;cursor:pointer'>X</span><span>Email can't be blank</span>`;
+          style='margin-right:40%;cursor:pointer'>X</span><span>Email can't be blank</span>`;
           $('#e-error').css("display","block");  
              return
        } else if(!filter.test(email)){
            document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-          style='margin-right:45%;cursor:pointer'>X</span><span> Invalid email id</span>`;
+          style='margin-right:40%;cursor:pointer'>X</span><span> Invalid email id</span>`;
           $('#e-error').css("display","block");  
              return
        }
        if(password=='' || password==undefined){
            document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-          style='margin-right:45%;cursor:pointer'>X</span><span>Password can't be blank</span>`;
+          style='margin-right:40%;cursor:pointer'>X</span><span>Password can't be blank</span>`;
           $('#e-error').css("display","block");  
               return
         }
        if(cpassword=='' || cpassword==undefined){
            document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-          style='margin-right:45%;cursor:pointer'>X</span><span>Confirm-password can't be blank</span>`;
+          style='margin-right:40%;cursor:pointer'>X</span><span>Confirm-password can't be blank</span>`;
           $('#e-error').css("display","block");    
               return
         }
 
          if(password!=cpassword){
               document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-          style='margin-right:45%;cursor:pointer'>X</span><span>password & confirm-password must be same </span>`;
+          style='margin-right:40%;cursor:pointer'>X</span><span>password & confirm-password must be same </span>`;
           $('#e-error').css("display","block");  
               return
         }
@@ -61,66 +69,92 @@ $(".save").click(function(){
       $('#email').val("")
       $('#password').val("")
       $('#re_password').val("")    
-          $('.sw-btn-next').show();
       }
 
 
                }
-           });
+       });
       });
-      var shopname=$('#shopname').val()
+
+    $('#shopname').mouseout(function(){
+        var shopname=$('#shopname').val()
+      if(shopname=='' || shopname==undefined){
+         document.getElementById('s-error').innerHTML=`<span class='error-icon' 
+         style='margin-right:40%;cursor:pointer'>X</span><span>Shop Name can't be blank</span>`;
+         $('#s-error').css("display","block");  
+         return
+     }  
+     })
+    $('#address1').mouseout(function(){
+      var address1=$('#address1').val()
+
+        if(address1=='' || address1==undefined){
+            document.getElementById('s-error').innerHTML=`<span class='error-icon' 
+            style='margin-right:40%;cursor:pointer'>X</span><span>Shop Address can't be blank</span>`;
+            $('#s-error').css("display","block");  
+            return
+        }
+       
+    })
+    $('#address2').mouseout(function(){
+      var address2=$('#address2').val()
+       
+    })
+    $('#city').mouseout(function(){
+        var city=$('#city').val()
+    
+      if(city =='' || city==undefined){
+            document.getElementById('s-error').innerHTML=`<span class='error-icon' 
+            style='margin-right:40%;cursor:pointer'>X</span><span>city can't be blank</span>`;
+            $('#s-error').css("display","block");  
+            return
+        }
+
+    })
+    $("#state").mouseout(function(){
+      var state=$('#state').val()
+
+    if(state =='' || state==undefined){
+        document.getElementById('s-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:40%;cursor:pointer'>X</span><span>State can't be blank</span>`;
+        $('#s-error').css("display","block");  
+        return
+        }
+
+    
+    })  
+    $('#code').mouseout(function(){
+      var code=$('#code').val()
+
+        if(code=='' || code==undefined){
+            document.getElementById('s-error').innerHTML=`<span class='error-icon' 
+            style='margin-right:40%;cursor:pointer'>X</span><span>Area code can't be blank</span>`;
+            $('#s-error').css("display","block");  
+            return
+        }
+        
+    })
+    $('#mobileNo').mouseout(function(){
+      var mobileNo=$('#mobileNo').val()
+
+       if(mobileNo=='' ||mobileNo ==undefined){
+        document.getElementById('s-error').innerHTML=`<span class='error-icon' 
+        style='margin-right:40%;cursor:pointer'>X</span><span>Mobile No can't be blank</span>`;
+        $('#s-error').css("display","block");  
+        return   
+       }
+    })
+      
+    $('.create-account').click(function(e){
+      var shopname=$('#shopname').val()  
       var address1=$('#address1').val()
       var address2=$('#address2').val()
       var city=$('#city').val()
       var state=$('#state').val()
       var code=$('#code').val()
       var mobileNo=$('#mobileNo').val()
- 
       var gst=$('#gst').val()
-
-      $('#shopname').keyup(function)
-      $
-    $('.create-account').click(function(e){
       e.preventDefault()
-
-     
-    if(shopname=='' || shopname==undefined){
-        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>Shop Name can't be blank</span>`;
-        $('#e-error').css("display","block");  
-        return
-    }
-    if(address1=='' || address1==undefined){
-        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>Shop Address can't be blank</span>`;
-        $('#e-error').css("display","block");  
-        return
-    }
-    if(city =='' || city==undefined){
-        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>city can't be blank</span>`;
-        $('#e-error').css("display","block");  
-        return
-    }
-    if(state =='' || state==undefined){
-        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>State can't be blank</span>`;
-        $('#e-error').css("display","block");  
-        return
-    }
-    if(code=='' || code==undefined){
-        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>Area code can't be blank</span>`;
-        $('#e-error').css("display","block");  
-        return
-    }
-    if(mobileNo =='' || mobileNo==undefined){
-        document.getElementById('e-error').innerHTML=`<span class='error-icon' 
-        style='margin-right:45%;cursor:pointer'>X</span><span>Mobile Number can't be blank</span>`;
-        $('#e-error').css("display","block");  
-        return
-    }
-
        $.ajax({
            url:'/shop',
            type:'POST',
